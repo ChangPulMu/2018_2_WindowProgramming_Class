fce1ee246254678d16fa531322d41ffb3d3bab22
ChangPulMu <>
2018-09-08 20:06:20 +0900

    Win32 활용 예제 마우스 및 키보드 동작에 따른 결과 출력 완료

--- Win32Practice1_1/Win32Pratice1_1.cpp
+++ Win32Practice1_1/Win32Pratice1_1.cpp
@@ -12,15 +12,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR IpszCmdL
 
 	WndClass.cbSize = sizeof(WNDCLASSEX);
 	WndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
-	WndClass.IpfnWndProc = WndProc;
+	WndClass.lpfnWndProc = WndProc;
 	WndClass.cbClsExtra = 0;
 	WndClass.cbWndExtra = 0;
 	WndClass.hInstance = hInstance;
 	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	WndClass.IpszMenuName = NULL;
-	WndClass.IpszClassName = szAppName;
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = szAppName;
 	WndClass.hIconSm = 0;
 
 	RegisterClassEx(&WndClass);
@@ -56,10 +56,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM IParam)
 	HDC hdc;
 	RECT rect;
 	PAINTSTRUCT ps;
+	POINT MousePoint;
 
 	LPTSTR szMsg1 = "I love Window Programming!";
 	LPTSTR szMsg2 = "키보드가 눌러졌습니다.";
 	LPTSTR szMsg3 = "키보드가 떼어졌습니다.";
+	LPTSTR szMsg4 = "마우스가 눌러졌습니다.";
+	LPTSTR szMsg5 = "마우스가 이동 중입니다.";
+	LPTSTR szMsg6 = new char[10];
 
 	switch (message)
 	{
@@ -84,6 +88,28 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM IParam)
 		break;
 	case WM_LBUTTONDBLCLK:
 		MessageBox(hwnd, "마우스 더블 클릭!", "마우스 메세지", MB_OK | MB_ICONASTERISK);
+		break;
+	case WM_LBUTTONDOWN:
+		MousePoint.x = LOWORD(IParam);
+		MousePoint.y = HIWORD(IParam);
+
+		hdc = GetDC(hwnd);
+		GetClientRect(hwnd, &rect);
+		wsprintf(szMsg6, "X:%ld, Y:%ld", MousePoint.x, MousePoint.y);
+
+		TextOut(hdc, MousePoint.x, MousePoint.y, szMsg6, strlen(szMsg6));
+		DrawText(hdc, szMsg4, strlen(szMsg4), &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		ReleaseDC(hwnd, hdc);
+		break;
+	case WM_LBUTTONUP:
+		InvalidateRect(hwnd, NULL, TRUE);
+		break;
+	case WM_MOUSEMOVE:
+		hdc = GetDC(hwnd);
+		GetClientRect(hwnd, &rect);
+		DrawText(hdc, szMsg5, strlen(szMsg5), &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		ReleaseDC(hwnd, hdc);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
